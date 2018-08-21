925d4c3dc0ff2aef6dbd601cc9b59e465b2e88f0
joel.macharia <joel@pezesha.com>
2018-08-21 21:06:19 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+"A simple hello world application"
+
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+ 
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
